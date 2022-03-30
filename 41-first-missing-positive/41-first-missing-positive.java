35ba3d25a3aabc7c929e0a7e4b98cdedf006546a
Matt Henslee <mhenslee@sevatec.com>
2022-03-29 22:16:59 -0500

    Time: 9 ms (24.06%), Space: 125.8 MB (75.29%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,17 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         
-        Arrays.sort(nums);        
+        Arrays.sort(nums); // O(n log n) 
         int n = 1;
         
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) { // O(n) solution
             if(nums[i] <= 0){
                 // negative number, skip it
                 continue;
             }
             else if(nums[i] == n){
                 if(i+1 < nums.length && nums[i+1] == nums[i]) {
-                    // check for multiples of the same integer                
+                    // check for multiples of the same integer
+                    // by looking at the next integer in array
                     continue;
                 }
                 else {
